cc253914df891c2ae9a43c0a1fd914236de3158f
AmanTiwari-0987 <amann.tiwari9285@gmail.com>
2026-06-08 12:35:20 +0530

    Day-2
    daily question

---
+++ DAY2Q1.c
@@ -0,0 +1,20 @@
+/*
+DAY-2 
+Question 1 
+MAX SUB ARRAY
+*/
+int maxSubArray(int* nums, int numsSize) {
+    int currentsum = 0 ;
+    int maxsum = nums[0] ;
+
+    for(int i = 0 ; i<numsSize ; i++){
+        currentsum += nums[i] ;
+        if(currentsum > maxsum){
+            maxsum = currentsum ;
+        }
+        if(currentsum < 0){
+            currentsum = 0 ;
+        }
+    }
+    return maxsum ;
+}
