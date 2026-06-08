73a2d3d004a85624865b356f2e40484529dcaceb
AmanTiwari-0987 <amann.tiwari9285@gmail.com>
2026-06-08 13:07:20 +0530

    Day 2

---
+++ D2Q2.py
@@ -0,0 +1,16 @@
+'''
+DAY 2 
+QUESTION 2 
+CONTAINS DUPLICATE
+'''
+class Solution(object):
+    def containsDuplicate(self, nums):
+        seen = set()
+
+        for num in nums :
+            if num in seen :
+                return True
+
+            seen.add(num)
+            
+        return False
